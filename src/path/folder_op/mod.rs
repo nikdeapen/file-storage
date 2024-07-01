81cd55f45f22938a4fcb65dc44c75f836988ce70
Nik Deapen <nik@deapen.com>
2024-07-01 06:06:52 -0700

    [local] Added support for temp files.

--- src/path/folder_op/mod.rs
+++ src/path/folder_op/mod.rs
@@ -1,2 +1,1 @@
-
 mod list_files;
--- src/path/imp/construction.rs
+++ src/path/imp/construction.rs
@@ -1,4 +1,8 @@
-use crate::{FolderPath, Path};
+use std::io::ErrorKind::Other;
+
+use tempfile::NamedTempFile;
+
+use crate::{FilePath, FolderPath, Path};
 
 impl Path {
     //! Roots
@@ -10,6 +14,68 @@ impl Path {
     }
 }
 
+impl Path {
+    //! Parse
+
+    /// Parses the path for known path structures.
+    ///
+    /// # Errors
+    /// Returns an error if the path is not part of a known file system.
+    pub fn parse<S>(path: S) -> Result<Self, std::io::Error>
+    where
+        S: AsRef<str> + Into<String>,
+    {
+        #[cfg(feature = "local")]
+        if crate::local::is_unix_path(path.as_ref()) {
+            return Ok(unsafe { Self::new_unchecked(path, 1, '/') });
+        }
+
+        #[cfg(feature = "local")]
+        if crate::local::is_windows_path(path.as_ref()) {
+            let b: &[u8] = path.as_ref().as_bytes();
+            debug_assert!(b.len() >= 3);
+            debug_assert!(b[2] == b'\\' || b[2] == b'/');
+            let file_separator: char = b[2] as char;
+            return Ok(unsafe { Self::new_unchecked(path, 3, file_separator) });
+        }
+
+        Err(std::io::Error::new(
+            Other,
+            format!("unknown file system for path: {}", path.as_ref()),
+        ))
+    }
+}
+
+impl Path {
+    //! Temp
+
+    /// Creates a local temp path.
+    #[cfg(feature = "local")]
+    pub fn temp() -> Result<Self, std::io::Error> {
+        let path: NamedTempFile = NamedTempFile::new()?;
+        let path: &str = path
+            .path()
+            .to_str()
+            .ok_or(std::io::Error::new(Other, "temp path not UTF-8"))?;
+        Self::parse(path)
+    }
+}
+
+impl FilePath {
+    //! Temp
+
+    /// Creates a local temp file.
+    #[cfg(feature = "local")]
+    pub fn temp() -> Result<Self, std::io::Error> {
+        Ok(Path::temp()?
+            .make_file("temp.file")
+            .ok_or(std::io::Error::new(
+                Other,
+                "for some reason 'e' is a file separator in this file system ¯\\_(ツ)_/¯",
+            ))?)
+    }
+}
+
 impl FolderPath {
     //! Roots
 
@@ -17,6 +83,16 @@ impl FolderPath {
     #[cfg(feature = "local")]
     pub fn unix_root() -> Self {
         Path::unix_root().to_folder().unwrap()
+    }
+}
+
+impl FolderPath {
+    //! Temp
+
+    /// Creates a local temp folder.
+    #[cfg(feature = "local")]
+    pub fn temp() -> Result<Self, std::io::Error> {
+        Ok(Path::temp()?.make_folder())
     }
 }
 
