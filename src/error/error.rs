f39307c29048f4bfa22cf2d72057f5a76e797d7e
Nik Deapen <nik@deapen.com>
2024-10-08 05:09:22 -0700

    [core] Added support for file & folder operation errors.

---
+++ src/error/error.rs
@@ -0,0 +1,126 @@
+use std::fmt::{Display, Formatter};
+use std::io;
+use std::io::ErrorKind;
+
+use crate::Reason::Other;
+use crate::StoragePath;
+use crate::{Operation, Reason};
+
+/// A file or folder operation error.
+#[derive(Debug)]
+pub struct Error {
+    path: StoragePath,
+    operation: Operation,
+    reason: Reason,
+    cause: Option<io::Error>,
+}
+
+impl Error {
+    //! Construction
+
+    /// Creates a new error.
+    pub fn new<P>(path: P, operation: Operation, reason: Reason) -> Self
+    where
+        P: Into<StoragePath>,
+    {
+        let path: StoragePath = path.into();
+        Self {
+            path,
+            operation,
+            reason,
+            cause: None,
+        }
+    }
+
+    /// Creates an error from the `cause`.
+    pub fn from_error<P, E>(path: P, operation: Operation, cause: E) -> Self
+    where
+        P: Into<StoragePath>,
+        E: Into<io::Error>,
+    {
+        let path: StoragePath = path.into();
+        Self {
+            path,
+            operation,
+            reason: Other,
+            cause: Some(cause.into()),
+        }
+    }
+
+    /// Creates an error from the `message`.
+    pub fn from_message<P, S>(path: P, operation: Operation, message: S) -> Self
+    where
+        P: Into<StoragePath>,
+        S: Into<String>,
+    {
+        Error::from_error(
+            path,
+            operation,
+            io::Error::new(ErrorKind::Other, message.into()),
+        )
+    }
+}
+
+impl Error {
+    //! Errors
+
+    /// Gets the `path not UTF-8` error.
+    pub fn path_not_utf8() -> io::Error {
+        io::Error::new(ErrorKind::Unsupported, "path is not UTF-8")
+    }
+
+    /// Gets the `unsupported file system` error.
+    pub fn unsupported_file_system(path: &str) -> io::Error {
+        io::Error::new(
+            ErrorKind::Unsupported,
+            format!("unsupported file system for path: {}", path),
+        )
+    }
+}
+
+impl Error {
+    //! Properties
+
+    /// Gets the path.
+    pub fn path(&self) -> &StoragePath {
+        &self.path
+    }
+
+    /// Gets the operation.
+    pub fn operation(&self) -> Operation {
+        self.operation
+    }
+
+    /// Gets the reason.
+    pub fn reason(&self) -> Reason {
+        self.reason
+    }
+
+    /// Gets the optional cause.
+    pub fn cause(&self) -> Option<&io::Error> {
+        self.cause.as_ref()
+    }
+}
+
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let file_or_folder: &str = if self.path.is_file() {
+            "file"
+        } else {
+            "folder"
+        };
+        write!(
+            f,
+            "{} error: op={} path={} ",
+            file_or_folder, self.operation, self.path
+        )?;
+        if let Some(cause) = self.cause() {
+            write!(f, "cause={}", cause)?;
+        } else {
+            write!(f, "reason={}", self.reason)?;
+        }
+        Ok(())
+    }
+}
+
+impl std::error::Error for Error {}
---
+++ src/error/reason.rs
@@ -0,0 +1,23 @@
+use std::fmt::{Display, Formatter};
+
+/// The reason for a file or folder operation error.
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+pub enum Reason {
+    /// The file system for the path could not be resolved.
+    UnknownFileSystem,
+
+    /// The file was not found. (this can also mean it unauthorized)
+    FileNotFound,
+
+    /// The file already exists.
+    FileAlreadyExists,
+
+    /// Another uncategorized error.
+    Other,
+}
+
+impl Display for Reason {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
