3d18d840a4772c45462aad988549229144d002d8
Nik Deapen <nik@deapen.com>
2024-11-18 19:53:54 -0800

    [path] Added support for file & folder paths.

---
+++ src/path/mod.rs
@@ -0,0 +1,10 @@
+pub use file_path::*;
+pub use folder_path::*;
+pub use storage_path::*;
+
+mod file_path;
+mod folder_path;
+mod storage_path;
+
+mod core;
+mod properties;
---
+++ src/path/properties/mod.rs
@@ -0,0 +1,2 @@
+mod base_and_extension;
+mod is_file_or_folder;
