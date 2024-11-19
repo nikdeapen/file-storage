f91095b243cb3e89023c1f7b4872dc1d9d2567fb
Nik Deapen <nik@deapen.com>
2024-11-19 04:35:15 -0800

    [exists] Added support for file existence checks.

---
+++ src/op/file/exists/exists.rs
@@ -0,0 +1,50 @@
+use std::io::ErrorKind::NotFound;
+
+use crate::Operation::Exists;
+use crate::Reason::UnknownFileSystem;
+use crate::{Error, FilePath};
+
+impl FilePath {
+    //! Exists
+
+    /// Checks if the file exists.
+    pub fn exists(&self) -> Result<bool, Error> {
+        if self.is_local_path() {
+            return self.exists_local();
+        }
+
+        Err(Error::new(self.clone(), Exists, UnknownFileSystem))
+    }
+}
+
+impl FilePath {
+    //! Local
+
+    fn exists_local(&self) -> Result<bool, Error> {
+        debug_assert!(self.is_local_path());
+
+        match std::fs::metadata(self) {
+            Ok(metadata) => {
+                if metadata.is_file() {
+                    Ok(true)
+                } else {
+                    let message: &str = if metadata.is_dir() {
+                        "the file path is a folder on the local file system"
+                    } else if metadata.is_symlink() {
+                        "the file path is a symlink on the local file system"
+                    } else {
+                        "the file path is an unknown entity on the local file system"
+                    };
+                    Err(Error::from_message(self.clone(), Exists, message))
+                }
+            }
+            Err(error) => {
+                if error.kind() == NotFound {
+                    Ok(false)
+                } else {
+                    Err(Error::from_cause(self.clone(), Exists, error))
+                }
+            }
+        }
+    }
+}
