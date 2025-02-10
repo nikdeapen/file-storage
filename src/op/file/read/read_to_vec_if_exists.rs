2aa931f1812836029f18130cb2f37dc16ac4c140
Nik Deapen <nik@deapen.com>
2025-02-10 11:15:28 -0800

    [read] Added support for reading files.

---
+++ src/op/file/read/read_to_vec_if_exists.rs
@@ -0,0 +1,20 @@
+use crate::system::LocalPath;
+use crate::Operation::Read;
+use crate::Reason::UnknownFileSystem;
+use crate::{Error, FilePath};
+
+impl FilePath {
+    //! Read to Vec If Exists
+
+    /// Reads the file to the `target` `Vec` if it exists.
+    ///
+    /// Returns `Ok(Some(file_content_len))`.
+    /// Returns `Ok(None)` if the file did not exist.
+    pub fn read_to_vec_if_exists(&self, target: &mut Vec<u8>) -> Result<Option<usize>, Error> {
+        if let Some(local) = LocalPath::from(self.path()) {
+            return local.read_to_vec_if_exists(target);
+        }
+
+        Err(Error::new(self.clone(), Read, UnknownFileSystem))
+    }
+}
---
+++ src/system/local/read.rs
@@ -0,0 +1,32 @@
+use std::io::ErrorKind::NotFound;
+use std::io::Read;
+
+use crate::system::LocalPath;
+use crate::{Error, Operation};
+
+impl<'a> LocalPath<'a> {
+    //! Read
+
+    //! See `FilePath::read_to_vec_if_exists
+    pub fn read_to_vec_if_exists(&self, mut target: &mut Vec<u8>) -> Result<Option<usize>, Error> {
+        debug_assert!(self.path.is_file());
+
+        match std::fs::File::open(self.path.as_str()) {
+            Ok(mut file) => {
+                let start: usize = target.len();
+                let read: usize = file.read_to_end(&mut target).map_err(|error| {
+                    Error::from_cause(self.path.clone(), Operation::Read, error)
+                })?;
+                debug_assert_eq!(target.len(), start + read);
+                Ok(Some(read))
+            }
+            Err(error) => {
+                if error.kind() == NotFound {
+                    Ok(None)
+                } else {
+                    Err(Error::from_cause(self.path.clone(), Operation::Read, error))
+                }
+            }
+        }
+    }
+}
