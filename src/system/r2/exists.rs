d6dfa7a767de5e53a2e19ad0410d5338b0265946
Nik Deapen <nik@deapen.com>
2025-04-27 08:56:19 -0700

    [r2] Added support for file existence checks.

---
+++ src/system/r2/exists.rs
@@ -0,0 +1,42 @@
+use crate::system::R2Path;
+use crate::Error;
+use crate::Operation::Exists;
+
+impl<'a> R2Path<'a> {
+    //! Exists
+
+    /// See `FilePath::exist`.
+    pub fn exists(&self) -> Result<bool, Error> {
+        tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(self.exists_async())
+    }
+
+    /// See `FilePath::exist`.
+    pub async fn exists_async(&self) -> Result<bool, Error> {
+        use aws_sdk_s3::error::SdkError;
+        use std::io;
+        use std::io::ErrorKind::Other;
+
+        let response = Self::get_client(self.account_id)
+            .await
+            .head_object()
+            .bucket(self.bucket)
+            .key(self.key)
+            .send()
+            .await;
+        match response {
+            Ok(_response) => Ok(true),
+            Err(error) => match error {
+                SdkError::ServiceError(e) if e.err().is_not_found() => Ok(false),
+                error => Err(Error::from_cause(
+                    self.path.clone(),
+                    Exists,
+                    io::Error::new(Other, error),
+                )),
+            },
+        }
+    }
+}
