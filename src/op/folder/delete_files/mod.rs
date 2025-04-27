c45ad5ed07dcd7fed71777d210a6cbadd44b0e13
Nik Deapen <nik@deapen.com>
2025-04-27 06:41:52 -0700

    [local] Added support for deleting files in folders.

--- src/op/folder/delete_files/mod.rs
+++ src/op/folder/delete_files/mod.rs
@@ -1,2 +1,1 @@
 mod delete_files;
-mod list_files;
---
+++ src/system/local/delete_files.rs
@@ -0,0 +1,22 @@
+use crate::system::LocalPath;
+use crate::Error;
+use crate::Operation::DeleteFiles;
+
+impl<'a> LocalPath<'a> {
+    //! Delete
+
+    /// See `FolderPath::delete_files`.
+    pub fn delete_files(&self) -> Result<(), Error> {
+        debug_assert!(self.path.is_folder());
+
+        let path: &str = self.path.as_str();
+        if std::fs::exists(path)
+            .map_err(|e| Error::from_cause(self.path.clone(), DeleteFiles, e))?
+        {
+            std::fs::remove_dir_all(path)
+                .map_err(|e| Error::from_cause(self.path.clone(), DeleteFiles, e))
+        } else {
+            Ok(())
+        }
+    }
+}
