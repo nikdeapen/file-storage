a29ff52a7456a2cb39a2a8b690dc01255178f94b
Nik Deapen <nik@deapen.com>
2025-08-26 09:25:30 -0700

    [error] Added support for error handling.

---
+++ src/error/reason.rs
@@ -0,0 +1,33 @@
+use std::fmt::{Display, Formatter};
+
+/// The reason for a file or folder operation error.
+#[derive(Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Debug)]
+pub enum Reason {
+    /// The file system could not be resolved.
+    UnknownFileSystem,
+
+    /// The file system does not support the operation.
+    UnsupportedOperation,
+
+    /// The file was not found.
+    FileNotFound,
+
+    /// The file already exists.
+    FileAlreadyExists,
+
+    /// Another uncategorized reason.
+    Other,
+}
+
+impl Display for Reason {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let s: &str = match self {
+            Reason::UnknownFileSystem => "unknown file system",
+            Reason::UnsupportedOperation => "unsupported operation",
+            Reason::FileNotFound => "file not found",
+            Reason::FileAlreadyExists => "file already exists",
+            Reason::Other => "other",
+        };
+        write!(f, "{}", s)
+    }
+}
