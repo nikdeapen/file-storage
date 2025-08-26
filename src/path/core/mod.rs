4629696f5cb1ca8e9b02ac0f24fa3dc958bae0ac
Nik Deapen <nik@deapen.com>
2025-08-26 09:23:37 -0700

    [path] Added support for path properties.

--- src/path/core/mod.rs
+++ src/path/core/mod.rs
@@ -1,1 +1,2 @@
 mod compare;
+mod display;
---
+++ src/path/properties/is_file_or_folder.rs
@@ -0,0 +1,35 @@
+use crate::StoragePath;
+
+impl StoragePath {
+    //! File or Folder
+
+    /// Checks if the path is a file. (if not, the path is a folder)
+    pub fn is_file(&self) -> bool {
+        !self.is_folder()
+    }
+
+    /// Checks if the path is a folder. (if not, the path is a file)
+    pub fn is_folder(&self) -> bool {
+        self.extension().is_empty() || self.path().ends_with(self.file_separator())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::StoragePath;
+
+    #[test]
+    fn is_file_or_folder() {
+        let path: StoragePath = unsafe { StoragePath::new("/the/path", 1, '/') };
+        assert!(path.is_file());
+        assert!(!path.is_folder());
+
+        let path: StoragePath = unsafe { StoragePath::new("/the/path/", 1, '/') };
+        assert!(!path.is_file());
+        assert!(path.is_folder());
+
+        let path: StoragePath = unsafe { StoragePath::new("!", 1, '/') };
+        assert!(!path.is_file());
+        assert!(path.is_folder());
+    }
+}
