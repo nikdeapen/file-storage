8d620508d79bd59538a9dc2b7abd0fa790c17f21
Nik Deapen <nik@deapen.com>
2026-03-06 05:29:18 -0800

    removed parent

--- src/path/properties/extension.rs
+++ src/path/properties/extension.rs
@@ -7,34 +7,45 @@ impl FilePath {
     ///
     /// Returns `None` if there is no `.` in the file name.
     pub fn extension(&self) -> Option<&str> {
-        let name: &str = self.file_name();
-        name.rfind('.').map(|pos| &name[pos + 1..])
+        self.dot_extension().map(|e| &e[1..])
+    }
+
+    /// Gets the file extension with the leading `.`.
+    ///
+    /// Returns `None` if there is no `.` in the file name.
+    pub fn dot_extension(&self) -> Option<&str> {
+        self.file_name().rfind('.').map(|pos| &self.file_name()[pos..])
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::FolderPath;
+    use crate::{FilePath, FolderPath};
     use std::error::Error;
 
     #[test]
     fn extension() -> Result<(), Box<dyn Error>> {
         let root: FolderPath = FolderPath::unix_root();
 
-        let file = root.clone().make_file("file.txt")?;
+        let file: FilePath = root.clone().make_file("file.txt")?;
         assert_eq!(file.extension(), Some("txt"));
+        assert_eq!(file.dot_extension(), Some(".txt"));
 
-        let file = root.clone().make_file("archive.tar.gz")?;
+        let file: FilePath = root.clone().make_file("archive.tar.gz")?;
         assert_eq!(file.extension(), Some("gz"));
+        assert_eq!(file.dot_extension(), Some(".gz"));
 
-        let file = root.clone().make_file("no_extension")?;
+        let file: FilePath = root.clone().make_file("no_extension")?;
         assert_eq!(file.extension(), None);
+        assert_eq!(file.dot_extension(), None);
 
-        let file = root.clone().make_file(".gitignore")?;
+        let file: FilePath = root.clone().make_file(".gitignore")?;
         assert_eq!(file.extension(), Some("gitignore"));
+        assert_eq!(file.dot_extension(), Some(".gitignore"));
 
-        let file = root.clone().make_file("trailing.")?;
+        let file: FilePath = root.clone().make_file("trailing.")?;
         assert_eq!(file.extension(), Some(""));
+        assert_eq!(file.dot_extension(), Some("."));
 
         Ok(())
     }
--- src/path/properties/mod.rs
+++ src/path/properties/mod.rs
@@ -4,5 +4,4 @@ mod file_name;
 mod is_file;
 mod is_folder;
 mod len;
-mod parent;
 mod relative_path;
